9f47c53d0ecbc9a55fca6bdaf3e8cfaea2dbe264
surendrasingh@Surendras-MacBook-Air.local <ss0622187@gmail.com>
2024-06-11 09:30:03 +0530

    mobile responsive remaining

--- fullstack/src/Navbar/Nav.jsx
+++ fullstack/src/Navbar/Nav.jsx
@@ -1,31 +1,71 @@
-import React from 'react'
-import './NavStyle.css'
+// import React from 'react'
+// import './NavStyle.css'
+// const Nav = () => {
+//   return (
+//     <div>
+//         <nav id='nav'>
+//             <div>
+
+//                 <div className='navfirst'>
+//                     <text>AI.GEN</text>
+//                     <div>
+//                         <p>Features</p>
+//                         <p>Roadmap</p>
+//                         <p>Tokenomics</p>
+//                     </div>
+//                 </div>
+//                 <div className='second'>
+//                     <button>
+//                         Whitepaper
+//                     </button>
+//                     <button>
+//                         Get Started
+//                     </button>
+//                 </div>
+//             </div>
+//         </nav>
+//     </div>
+//   )
+// }
+
+// export default Nav
+
+import React from 'react';
+import './NavStyle.css';
+
 const Nav = () => {
   return (
     <div>
-        <nav id='nav'>
+      <nav id='nav'>
+        <div className='nav-container'>
+          <div className='navfirst'>
+            <text>AI.GEN</text>
             <div>
-
-                <div className='navfirst'>
-                    <text>AI.GEN</text>
-                    <div>
-                        <p>Features</p>
-                        <p>Roadmap</p>
-                        <p>Tokenomics</p>
-                    </div>
-                </div>
-                <div className='second'>
-                    <button>
-                        Whitepaper
-                    </button>
-                    <button>
-                        Get Started
-                    </button>
-                </div>
+              <p>Features</p>
+              <p>Roadmap</p>
+              <p>Tokenomics</p>
             </div>
-        </nav>
+          </div>
+          <div className='second'>
+            <button>
+              Whitepaper
+            </button>
+            <button>
+              Get Started
+            </button>
+          </div>
+        </div>
+        {/* <div className='nav-mobile'>
+          <text>AI.GEN</text>
+          <div className='hamburger-menu'>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div> */}
+      </nav>
     </div>
   )
 }
 
-export default Nav
+export default Nav;
